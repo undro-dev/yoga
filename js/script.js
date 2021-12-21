2d0322a4e9a5334211f5955c6255517d74735dce
Vitaly Undro <vitalyundro@gmail.com>
2021-12-21 22:03:52 +0300

    feat: add  slider

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ window.addEventListener('DOMContentLoaded', function () {
     }
   });
   //timer
-  let deadLine = '2021-12-12';
+  let deadLine = '2021-12-31';
 
   function getTimeRemaining(endtime) {
     let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -143,6 +143,59 @@ window.addEventListener('DOMContentLoaded', function () {
 
     for (let i = 0; i < input.length; i++) {
       input[i].value = '';
+    }
+  });
+  //slider
+
+  let slideIndex = 1,
+    slides = document.querySelectorAll('.slider-item'),
+    prev = document.querySelector('.prev'),
+    next = document.querySelector('.next'),
+    dotsWrap = document.querySelector('.slider-dots'),
+    dots = document.querySelectorAll('.dot');
+
+  showSlides(slideIndex);
+
+  function showSlides(n) {
+
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+
+    slides.forEach((item) => item.style.display = 'none');
+
+    dots.forEach((item) => item.classList.remove('dot-active'));
+
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].classList.add('dot-active');
+
+  }
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  prev.addEventListener('click', function () {
+    plusSlides(-1);
+  });
+
+  next.addEventListener('click', function () {
+    plusSlides(1);
+  });
+
+  dotsWrap.addEventListener('click', (event) => {
+    for (let i = 0; i < dots.length + 1; i++) {
+      if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
+        currentSlide(i);
+      }
     }
   });
 });
